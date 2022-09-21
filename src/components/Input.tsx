3933e074156e78083f020e4f1830fc1587bd9574
BlakeIsMyWaifu <33188727+BlakeIsMyWaifu@users.noreply.github.com>
2022-09-21 09:26:51 +0100

    ✨ God input

---
+++ src/components/Input.tsx
@@ -0,0 +1,67 @@
+import { ActionIcon, Autocomplete, Avatar, Group, Text, type AutocompleteItem } from '@mantine/core'
+import { IconArrowRight, IconSearch } from '@tabler/icons'
+import { FC, forwardRef, useState } from 'react'
+
+export interface InputData extends AutocompleteItem {
+	icon: string;
+}
+
+interface InputProps {
+	label: string;
+	placeholder: string;
+	data: InputData[];
+	limit?: number;
+	onSubmit: (value: string) => void;
+}
+
+const Input: FC<InputProps> = ({ label, placeholder, data, limit = 6, onSubmit }) => {
+
+	const AutoCompleteItem = forwardRef<HTMLDivElement, InputData>(function Test({ value, icon, ...other }: InputData, ref) {
+		return (
+			<div ref={ref} {...other}>
+				<Group noWrap>
+					<Avatar src={icon} />
+					<Text>{value}</Text>
+				</Group>
+			</div>
+		)
+	})
+
+	const [value, setValue] = useState('')
+
+	const handleSubmit = (): void => {
+		const inputText = value.toLowerCase()
+		if (!inputText || !data.filter(god => god.value.toLowerCase() === inputText).length) return
+		onSubmit(inputText)
+	}
+
+	return <Autocomplete
+		label={label}
+		placeholder={placeholder}
+		icon={<IconSearch />}
+		data={data}
+		itemComponent={AutoCompleteItem}
+		limit={limit}
+		size='xl'
+		transition='pop-top-left'
+		transitionDuration={80}
+		transitionTimingFunction='ease'
+		rightSection={
+			<ActionIcon
+				size={32}
+				onMouseDown={handleSubmit}
+			>
+				<IconArrowRight size={32} stroke={1.5} />
+			</ActionIcon>
+		}
+		rightSectionWidth={48}
+		onKeyDown={event => {
+			if (event.key !== 'Enter') return
+			handleSubmit()
+		}}
+		value={value}
+		onChange={setValue}
+	/>
+}
+
+export default Input
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,24 @@
+import { Center } from '@mantine/core'
+import Input, { type InputData } from 'components/Input'
+import { godData } from 'data/gods'
 import type { NextPage } from 'next'
+import { useMemo } from 'react'
 
 const Home: NextPage = () => {
+
+	const data: InputData[] = useMemo(() => godData.map(god => ({ value: god.Name, icon: god.godIcon_URL })), [])
+
 	return (
-		<p>Hello World</p>
+		<Center style={{
+			height: '40vh'
+		}}>
+			<Input
+				label='Guess a God'
+				placeholder='Enter God Name . . .'
+				data={data}
+				onSubmit={value => console.log(value)}
+			/>
+		</Center>
 	)
 }
 
